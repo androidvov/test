d840c07d261cfd319353662934ec265f489f27ec
zhaoshuai <zhaoshuaivov@163.com>
2015-01-08 23:34:46 +0800

    can I add this File

--- Test.java
+++ Test.java
@@ -3,5 +3,23 @@ public class Test{
 	public static void main(String args[]){
 
 		System.out.println("Here is what i will show for you !")
-	}
+		
+		
+		
+		
+			}
+			
+			
+
+			//add some method in here
+			public void getUserName(){
+				
+				return "name";
+			}
+			
+			
+			public void setUserName(String name){
+				
+					System.out.println(name);
+			}
 }
